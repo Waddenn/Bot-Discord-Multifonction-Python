5f8f74fa490b87da4299660a74f8ccd7f2f633dd
Wade <115143365+Waddenn@users.noreply.github.com>
2023-10-19 11:52:45 +0200

    adding welcome_new_member

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import discord
-
 from config.private import TOKEN
+from app.utils.salutations import welcome_new_member
+
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
+intents.members = True
 
-client = discord.Client(intents=intents)
+bot = discord.Client(intents=intents)
 
-client.run(TOKEN)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} est connecté au serveur")
+
+
+@bot.event
+async def on_member_join(member):
+    await welcome_new_member(bot, member)
+
+
+bot.run(TOKEN)
