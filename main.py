974744c4a1958e7ffae227688e06074e37db6e50
Wade <115143365+Waddenn@users.noreply.github.com>
2023-10-19 12:02:04 +0200

    adding sondage

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import discord
+from discord.ext import commands
 from config.private import TOKEN
 from app.utils.salutations import welcome_new_member
-
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 intents.members = True
+intents.message_content = True
 
-bot = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 @bot.event
@@ -19,6 +20,14 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     await welcome_new_member(bot, member)
+
+
+@bot.command()
+async def sondage(ctx, *, question):
+    poll_message = await ctx.send(f"**Sondage :** {question}")
+    await poll_message.add_reaction("👍")
+    await poll_message.add_reaction("👎")
+    await poll_message.add_reaction("🤷")
 
 
 bot.run(TOKEN)
